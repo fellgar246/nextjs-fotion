8dacf65e9340cf768c1c884ed1ef12c63e13e78e
Luis Felipe Garcia <fellgar246@gmail.com>
2024-02-02 20:10:05 -0600

    create notes

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { ConvexClientProvider } from "@/components/providers/convex-provider";
+import { Toaster } from "sonner"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -42,7 +43,8 @@ export default function RootLayout({
             disableTransitionOnChange
             storageKey="fotion-theme"
           >
-          {children}
+            <Toaster position="bottom-center" />
+            {children}
           </ThemeProvider>
         </ConvexClientProvider>
       </body>
