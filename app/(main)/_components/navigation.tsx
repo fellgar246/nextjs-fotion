b3e814e677597f2c16aa0bea658741df708ccd18
Luis Felipe Garcia <fellgar246@gmail.com>
2024-02-04 22:35:48 -0600

    add trash box

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -5,7 +5,10 @@ import {
   MenuIcon, 
   PlusCircle, 
   Search, 
-  Settings } from "lucide-react"
+  Settings,
+  Plus,
+  Trash 
+} from "lucide-react"
 import { usePathname } from "next/navigation"
 import { ElementRef, useEffect, useRef, useState } from "react"
 import { useMediaQuery } from "usehooks-ts"
@@ -13,10 +16,17 @@ import { useMutation } from "convex/react"
 import { toast } from "sonner"
 
 import { cn } from "@/lib/utils"
-import { UserItem } from "./user-item"
 import { api } from "@/convex/_generated/api"
+import { 
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+} from "@/components/ui/popover"
+
+import { UserItem } from "./user-item"
 import { Item } from "./item"
 import { DocumentList } from "./document-list"
+import { TrashBox } from "./trash-box"
 
 export const Navigation = () => {
     const pathName = usePathname()
@@ -155,6 +165,22 @@ export const Navigation = () => {
                 </div>
                 <div className="mt-4">
                     <DocumentList />
+                    <Item 
+                      onClick={handleCreate}
+                      icon={Plus}
+                      label="Add a page" 
+                    />
+                    <Popover>
+                      <PopoverTrigger className="w-full mt-4">
+                        <Item label="Trash" icon={Trash} />
+                      </PopoverTrigger>
+                      <PopoverContent
+                        className="p-0 w-72"
+                        side={isMobile ? "bottom" : "right"}
+                      > 
+                        <TrashBox />
+                      </PopoverContent>
+                    </Popover>
                 </div>
                 <div 
                     onMouseDown={handleMouseDown}
---
+++ app/(main)/_components/trash-box.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+export const TrashBox = () => { 
+    return (
+        <div>
+            <h1>TrashBox</h1>
+        </div>
+    )
+}
