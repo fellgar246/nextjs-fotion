feef4214b7047f19ea5370eec3dff3d00b2c8648
Luis Felipe Garcia <fellgar246@gmail.com>
2024-02-10 13:57:14 -0600

    title edit

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -9,7 +9,7 @@ import {
   Plus,
   Trash 
 } from "lucide-react"
-import { usePathname } from "next/navigation"
+import { usePathname, useParams } from "next/navigation"
 import { ElementRef, useEffect, useRef, useState } from "react"
 import { useMediaQuery } from "usehooks-ts"
 import { useMutation } from "convex/react"
@@ -29,10 +29,12 @@ import { UserItem } from "./user-item"
 import { Item } from "./item"
 import { DocumentList } from "./document-list"
 import { TrashBox } from "./trash-box"
+import { Navbar } from "./navbar"
 
 export const Navigation = () => {
   const settings = useSettings()
     const search = useSearch()
+    const params = useParams()
     const pathName = usePathname()
     const isMobile = useMediaQuery("(max-width: 768px)")
     const create = useMutation(api.documents.create)
@@ -200,9 +202,17 @@ export const Navigation = () => {
                     isMobile && "left-0 w-full"
                 )}
             >
+              {!!params.documentId ? (
+                <Navbar 
+                  isCollapsed={isCollapsed}
+                  onResetWidth={resetWidth}
+                />
+              ): (
                 <nav className="bg-transparent px-3 py-2 w-full">
                     {isCollapsed && <MenuIcon onClick={resetWidth} role="button" className="h-6 w-6 text-muted-foreground" />}
                 </nav>
+              )}
+                
             </div>
         </>
     )
